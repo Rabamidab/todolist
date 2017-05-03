06e1ed68248049b74323f91f4fd8306740ca7f90
Rabamidab <rabamidab@gmail.com>
2017-05-03 10:59:17 +0300

    create environment
    Тут работает babel и scss

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import './style.scss'
+const path = require('./twice');
+console.log('Hello webpack 2');
+let a = 3;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+  	path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  watch: true,
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['es2015'] }
+      }]
+    },
+    {
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }
+  ]
+}
+}
