247e37db66848c36364f1dfddda782628ff71079
Rabamidab <rabamidab@gmail.com>
2017-05-04 09:15:41 +0300

    add model
    Добавил модель пункта в списке дел, и коллекцию пунктов

--- src/app.js
+++ src/app.js
@@ -1,4 +1,33 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+import _ from 'underscore';
 import './style.scss'
-const path = require('./twice');
-console.log('Hello webpack 2');
-let a = 3;
+// const path = require('./twice');
+// console.log('Hello webpack 2');
+// let a = 3;
+
+const ListItem = Backbone.Model.extend({
+    defaults: {
+        description: '',
+        deadline: 10,
+        is_checked: false
+    }
+});
+
+const ListItemsCollection = Backbone.Collection.extend({  
+    model: ListItem
+});
+
+const todolist = new ListItemsCollection;
+
+todolist.comparator = function(listitem) {
+    return listitem.get('deadline');
+};
+
+todolist.add(new ListItem({ description: 'Побегать', deadline: 5 }));
+todolist.add(new ListItem({ description: 'Попрыгать', deadline: 1 }));
+todolist.add(new ListItem({ description: 'Сделать уроки', deadline: 3 }));
+todolist.add(new ListItem({ description: 'Погонять голубей', deadline: 2 }));
+todolist.add(new ListItem({ description: 'Починить самолёт', deadline: 9 }));
+
+console.log(todolist.pluck('description'));
